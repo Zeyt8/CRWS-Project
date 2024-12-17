ef1778a3db823507afaedf02375727e9519e31ea
Sean Cutajar <cutajarsean0@gmail.com>
2024-12-17 18:51:17 +0100

    Update to Level 2
    Update to Level 2 economy system, still a work in progress

---
+++ Assets/Level2 Assets/Scripts/EnemyCastleHP.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyCastleHP : MonoBehaviour
+{
+
+    public int EnemyHealth = 1000;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
