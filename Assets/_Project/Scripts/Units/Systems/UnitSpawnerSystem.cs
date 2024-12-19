8abe68db00737110c0eca3c1bae4476b72f19066
ayinzhang <1242857339@qq.com>
2024-12-19 19:21:53 +0100

    Apply NavMesh in ECS

--- Assets/_Project/Scripts/Units/Systems/UnitSpawnerSystem.cs
+++ Assets/_Project/Scripts/Units/Systems/UnitSpawnerSystem.cs
@@ -19,10 +19,23 @@ partial struct UnitSpawnerSystem : ISystem
             if (unitSpawner.UnitToSpawn.HasValue)
             {
                 Entity unit = state.EntityManager.Instantiate(unitSpawner.UnitPrefabEntity);
-                SystemAPI.SetComponent(unit, LocalTransform.FromPosition(new float3(0, 0, 0)));
+
+                float3 spawnPosition = new float3(0, 0, 0);
+                SystemAPI.SetComponent(unit, LocalTransform.FromPosition(spawnPosition));
+
+                float3 targetPosition = new float3(-3, 0, -200);
+
+                if (!SystemAPI.HasBuffer<PathBufferElement>(unit))
+                    state.EntityManager.AddBuffer<PathBufferElement>(unit);
+
+                var pathBuffer = SystemAPI.GetBuffer<PathBufferElement>(unit);
+                NavMeshUtility.CalculatePath(spawnPosition, targetPosition, pathBuffer);
+
                 Movement ms = SystemAPI.GetComponent<Movement>(unit);
-                ms.Target = new float3(100, 0, 0);
+                ms.Target = targetPosition;
+                ms.CurrentPathIndex = 0; 
                 SystemAPI.SetComponent(unit, ms);
+
                 unitSpawner.UnitToSpawn = null;
                 SystemAPI.SetSingleton(unitSpawner);
             }
