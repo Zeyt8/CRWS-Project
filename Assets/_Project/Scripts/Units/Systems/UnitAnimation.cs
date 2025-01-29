b5d5b5845198a13368b6b0f5164c6043281aa048
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2025-01-29 23:11:21 +0100

    Vertex animations

--- Assets/_Project/Scripts/Units/Systems/UnitAnimation.cs
+++ Assets/_Project/Scripts/Units/Systems/UnitAnimation.cs
@@ -1,28 +1,41 @@
-using Latios.Mimic.Addons.Mecanim;
+using AnimCooker;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 partial struct UnitAnimationSystem : ISystem
 {
+    void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<AnimDbRefData>();
+    }
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        UnitAnimationJob job = new UnitAnimationJob();
+        UnitAnimationJob job = new UnitAnimationJob()
+        {
+            Db = SystemAPI.GetSingleton<AnimDbRefData>()
+        };
         state.Dependency = job.ScheduleParallel(state.Dependency);
     }
 
     [BurstCompile]
     partial struct UnitAnimationJob : IJobEntity
     {
-        public void Execute(MecanimAspect anim, in MovementData mov, ref AttackerData attacker)
+        [ReadOnly] public AnimDbRefData Db;
+
+        [BurstCompile]
+        public void Execute(ref AnimationCmdData cmd, in MovementData mov, ref AttackerData attacker, in AnimationStateData state)
         {
-            anim.SetBool(0, mov.IsMoving);
-            anim.SetFloat(2, mov.CurrentVelocity);
-            if (attacker.AttackAnimTrigger)
-            {
-                anim.SetTrigger(3);
-                attacker.AttackAnimTrigger = false;
-            }
+            /*anim.SetBool(0, mov.IsMoving);
+                anim.SetFloat(2, mov.CurrentVelocity);
+                if (attacker.AttackAnimTrigger)
+                {
+                    anim.SetTrigger(3);
+                    attacker.AttackAnimTrigger = false;
+                }*/
         }
+
     }
 }
