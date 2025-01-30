444b1d0565313c210d7dafc75731a366a97d2fa9
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2025-01-30 11:02:43 +0100

    Full state machine. Weapon animations

--- Assets/_Project/Scripts/Units/Systems/UnitAnimation.cs
+++ Assets/_Project/Scripts/Units/Systems/UnitAnimation.cs
@@ -7,12 +7,21 @@ using UnityEngine;
 
 partial struct UnitAnimationSystem : ISystem
 {
+    private ComponentLookup<AnimationStateData> _animationStateLookup;
+
     [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        _animationStateLookup = state.GetComponentLookup<AnimationStateData>();
+    }
+
     public void OnUpdate(ref SystemState state)
     {
+        _animationStateLookup.Update(ref state);
         EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.TempJob);
         UnitAnimationJob job = new UnitAnimationJob()
         {
+            AnimationStateLookup = _animationStateLookup,
             ECB = ecb.AsParallelWriter()
         };
         state.Dependency = job.ScheduleParallel(state.Dependency);
@@ -21,45 +30,56 @@ partial struct UnitAnimationSystem : ISystem
         ecb.Dispose();
     }
 
-    [BurstCompile]
     partial struct UnitAnimationJob : IJobEntity
     {
+        [ReadOnly] public ComponentLookup<AnimationStateData> AnimationStateLookup;
         public EntityCommandBuffer.ParallelWriter ECB;
 
-        [BurstCompile]
         public void Execute([EntityIndexInQuery] int entityInQueryIndex, in MovementData mov, ref AttackerData attacker, DynamicBuffer<Child> children)
         {
             foreach (Child child in children)
             {
                 Entity childEntity = child.Value;
-
-                if (mov.IsMoving)
+                if (!AnimationStateLookup.HasComponent(childEntity))
                 {
-                    ECB.SetComponent(entityInQueryIndex, childEntity, new AnimationCmdData
+                    continue;
+                }
+                AnimationStateData animState = AnimationStateLookup.GetRefRO(childEntity).ValueRO;
+
+                AnimationCmdData cmdData = new AnimationCmdData();
+                cmdData.Speed = 1;
+                if (attacker.AttackAnimTrigger)
+                {
+                    cmdData.Cmd = AnimationCmd.PlayOnce;
+                    cmdData.ClipIndex = (byte)TwoHanded.Attack1;
+                    attacker.AttackAnimTrigger = false;
+                }
+                else if (mov.IsMoving)
+                {
+                    cmdData.Cmd = AnimationCmd.SetPlayForever;
+                    if (mov.CurrentVelocity > 0.66f)
                     {
-                        Cmd = AnimationCmd.SetPlayForever,
-                        ClipIndex = (byte)TwoHanded.Walk
-                    });
+                        cmdData.ClipIndex = (byte)TwoHanded.Run;
+                    }
+                    else if (mov.CurrentVelocity > 0.33f)
+                    {
+                        cmdData.ClipIndex = (byte)TwoHanded.Walk;
+                    }
+                    else
+                    {
+                        cmdData.ClipIndex = (byte)TwoHanded.WalkSlow;
+                    }
                 }
                 else
                 {
-                    ECB.SetComponent(entityInQueryIndex, childEntity, new AnimationCmdData
-                    {
-                        Cmd = AnimationCmd.SetPlayForever,
-                        ClipIndex = (byte)TwoHanded.Idle
-                    });
+                    cmdData.Cmd = AnimationCmd.SetPlayForever;
+                    cmdData.ClipIndex = (byte)TwoHanded.Idle;
                 }
-
-                // Your animation logic here
-                /*anim.SetBool(0, mov.IsMoving);
-                anim.SetFloat(2, mov.CurrentVelocity);
-                if (attacker.AttackAnimTrigger)
+                if (cmdData.ClipIndex != animState.CurrentClipIndex)
                 {
-                    anim.SetTrigger(3);
-                    attacker.AttackAnimTrigger = false;
-                }*/
+                    ECB.SetComponent(entityInQueryIndex, childEntity, cmdData);
+                }
             }
         }
-
     }
 }
