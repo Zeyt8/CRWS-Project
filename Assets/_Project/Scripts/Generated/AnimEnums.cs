4d7275ae33d9aaec31f48400a08e56e6d311db96
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2025-01-31 02:56:42 +0100

    Finish animations

--- Assets/_Project/Scripts/Generated/AnimEnums.cs
+++ Assets/_Project/Scripts/Generated/AnimEnums.cs
@@ -1,12 +1,16 @@
-public enum Model : short { Brute, Crossbow, Hammer, Karate, Knight, Mage, Ninja, Sorceress, Spearman, Swordsman, Archer, TwoHanded }
+public enum Model : short { TwoHanded, Hammer, Archer, Brute, Crossbow, Karate, Knight, Mage, Ninja, Sorceress, Spearman, Swordsman }
 public static class ModelSize { public const short Size = 12; }
 
+public enum TwoHanded : byte { Attack1, Idle, WalkSlow, Walk, Run }
+public static class TwoHandedSize { public const byte Size = 5; }
+public enum Hammer : byte { Attack1, Idle, WalkSlow, Walk, Run }
+public static class HammerSize { public const byte Size = 5; }
+public enum Archer : byte { Attack1, Idle1, WalkSlow, Walk, Run }
+public static class ArcherSize { public const byte Size = 5; }
 public enum Brute : byte { Attack1, Idle, WalkSlow, Walk, Run }
 public static class BruteSize { public const byte Size = 5; }
 public enum Crossbow : byte { Attack1, Idle, WalkSlow, Walk, Run }
 public static class CrossbowSize { public const byte Size = 5; }
-public enum Hammer : byte { Attack1, Idle, WalkSlow, Walk, Run }
-public static class HammerSize { public const byte Size = 5; }
 public enum Karate : byte { Attack1, Idle, WalkSlow, Walk, Run }
 public static class KarateSize { public const byte Size = 5; }
 public enum Knight : byte { Attack1, Idle, WalkSlow, Walk, Run }
@@ -21,7 +25,3 @@ public enum Spearman : byte { Attack1, Idle, WalkSlow, Walk, Run }
 public static class SpearmanSize { public const byte Size = 5; }
 public enum Swordsman : byte { Attack1, Idle, WalkSlow, Walk, Run }
 public static class SwordsmanSize { public const byte Size = 5; }
-public enum Archer : byte { Attack1, Idle1, WalkSlow, Walk, Run }
-public static class ArcherSize { public const byte Size = 5; }
-public enum TwoHanded : byte { Attack1, Idle, WalkSlow, Walk, Run }
-public static class TwoHandedSize { public const byte Size = 5; }
